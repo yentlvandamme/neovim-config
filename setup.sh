00d59b5b9db1c5e293a7c14bfeec3f4d197fcd4d
yentlvandamme <yentl.van.damme@gmail.com>
2023-07-25 13:50:41 +0200

    Write setup shell script

---
+++ setup.sh
@@ -0,0 +1,11 @@
+echo "Initializing Neovim config"
+
+config_target=~/.config/nvim
+source=./Neovim/.
+
+echo "Target location of config:" $config_target
+echo "Source of config:" $source
+
+rm -r $config_target
+mkdir $config_target
+cp -R $source $config_target
