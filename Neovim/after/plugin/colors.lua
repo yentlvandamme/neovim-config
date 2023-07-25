cce2acd5fb67f725e7f8a3d1c3847233c73f82a9
yentlvandamme <yentl.van.damme@gmail.com>
2023-07-25 12:50:29 +0200

    Add current config

---
+++ Neovim/after/plugin/colors.lua
@@ -0,0 +1,9 @@
+require('rose-pine').setup()
+
+function ColorMyPencils(color)
+	color = color or "rose-pine"
+	vim.cmd.colorscheme(color)
+
+end
+
+ColorMyPencils()
---
+++ Neovim/after/plugin/nvim-tree.lua
@@ -0,0 +1,4 @@
+require("nvim-tree").setup()
+
+-- TODO:
+-- [] set a keyboard shortcut which toggles the tree view (nvim-tree.api.tree.toggle())
