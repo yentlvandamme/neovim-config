d7e3e94b286e16daf385925c5d938dec40d5dcff
yentlvandamme <yentl.van.damme@gmail.com>
2024-04-30 18:48:33 -0400

    Create packages folder

--- main.go
+++ main.go
@@ -8,62 +8,8 @@ import (
 	"os"
 	"path/filepath"
 	"runtime"
+    "neovim-config/distribution"
 )
-
-type Config struct {
-	Name        string
-	Version     string
-	Description string
-}
-
-type Distribution struct {
-	path   string
-	config Config
-}
-
-func FindDist(name string) (Distribution, error) {
-	distribution := Distribution{}
-	config := Config{}
-
-	currentDir, getwdErr := os.Getwd()
-	if getwdErr != nil {
-		return distribution, getwdErr
-	}
-
-	distsPath := currentDir + "/distributions"
-	dists, readDirErr := os.ReadDir(distsPath)
-	if readDirErr != nil {
-		return distribution, readDirErr
-	}
-
-	for _, dist := range dists {
-		if dist.IsDir() {
-			distPath := distsPath + string(os.PathSeparator) + dist.Name()
-			manifestUri := distPath + string(os.PathSeparator) + "manifest.json"
-			if _, fileNotExistErr := os.Stat(manifestUri); errors.Is(fileNotExistErr, os.ErrNotExist) {
-				continue
-			}
-
-			data, readFileErr := os.ReadFile(manifestUri)
-			if readFileErr != nil {
-				return distribution, readFileErr
-			}
-
-			if unmarshalErr := json.Unmarshal(data, &config); unmarshalErr != nil {
-				return distribution, unmarshalErr
-			}
-
-			if config.Name == name {
-				distribution.config = config
-				distribution.path = distPath
-				return distribution, nil
-			}
-		}
-	}
-
-	return distribution, fmt.Errorf("Could not find matching configuration")
-}
-
 
 func main() {
 	cmd := os.Args[1]
